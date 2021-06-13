1923b618296a9ce141b4590c2d8ba893f637ee1f
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-13 21:52:46 +0930

    added heroku placeholder and local host info

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
-    port: process.env.DB_PORT,
-});
+let sequelize;
+
+// heroku placeholder
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
