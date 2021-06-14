d3525859663f83db6dd3b8576c304cad3ad0303b
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-14 18:46:20 +0930

    changed user id

--- models/post.js
+++ models/post.js
@@ -4,41 +4,43 @@ const sequelize = require('../config/connection');
 class Post extends Model {}
 
 Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            // TEXT has unlimited characters - STRING has a limit of characters allowed
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
-    post_title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    post_content: {
-        // TEXT has unlimited characters - STRING has a limit of characters allowed
-      type: DataTypes.TEXT,
-    },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post',
-  }
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
 );
 
 module.exports = Post;
